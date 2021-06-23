1b9f1e1d953471bf6bdb8619be86031ff4d19039
wyattlake <wattttte@gmail.com>
2021-06-22 21:26:00 -0700

    Restructured training

--- model.py
+++ model.py
@@ -10,7 +10,6 @@ class BertSST(pl.LightningModule):
             cfg.checkpoint, num_labels=5)
         self.criterion = torch.nn.CrossEntropyLoss()
         self.lr = cfg.learning_rate
-        self.batch_size = cfg.batch_size
         self.run = run
 
     def configure_optimizers(self):
@@ -21,32 +20,49 @@ class BertSST(pl.LightningModule):
         embedding = self.encoder(x)
         return embedding
 
-    def training_step(self, batch, _batch_idx):
-        x, y = batch
-        logits = self.model(x).logits
-        loss = self.criterion(logits, y.long())
+    def training_step(self, batch, batch_idx):
+        inputs, targets = batch
+        logits = self.model(inputs).logits
+        loss = self.criterion(logits, targets.long())
 
         # Accuracy calculations
-        pred_labels = torch.argmax(logits, axis=1)
-        batch_acc = (pred_labels == y).sum().item()
+        preds = torch.argmax(logits, axis=1)
+        acc = self.calc_acc(preds, targets)
 
         # Neptune logging
         self.run['train/loss'].log(loss.item() / self.batch_size)
-        self.run['train/acc'].log(batch_acc / self.batch_size)
+        self.run['train/acc'].log(acc)
 
-        return loss
+        return {'loss': loss, 'preds': preds, 'targets': targets}
 
-    def validation_step(self, batch, _batch_idx):
-        x, y = batch
-        logits = self.model(x).logits
-        loss = self.criterion(logits, y.long())
-
-        # Accuracy calculations
-        pred_labels = torch.argmax(logits, axis=1)
-        batch_acc = (pred_labels == y).sum().item()
+    def training_epoch_end(self, outputs):
+        # Evaluating epoch averages
+        preds = torch.cat([x['preds'] for x in outputs])
+        targets = torch.cat([x['targets'] for x in outputs])
+        loss = torch.stack([x['loss'] for x in outputs]).mean()
 
         # Neptune logging
-        self.run['eval/loss'].log(loss.item() / self.batch_size)
-        self.run['eval/acc'].log(batch_acc / self.batch_size)
+        self.run['train_epoch/loss'].log(loss.item())
+        self.run['train_epoch/acc'].log(self.calc_acc(preds, targets))
 
-        return loss
+    def validation_step(self, batch, batch_idx):
+        inputs, targets = batch
+        logits = self.model(inputs).logits
+        loss = self.criterion(logits, targets.long())
+
+        preds = torch.argmax(logits, axis=1)
+
+        return {'loss': loss, 'preds': preds, 'targets': targets}
+
+    def validation_epoch_end(self, outputs):
+        # Evaluating epoch averages
+        preds = torch.cat([x['preds'] for x in outputs])
+        targets = torch.cat([x['targets'] for x in outputs])
+        loss = torch.stack([x['loss'] for x in outputs]).mean()
+
+        # Neptune logging
+        self.run['eval_epoch/loss'].log(loss.item())
+        self.run['eval_epoch/acc'].log(self.calc_acc(preds, targets))
+
+    def calc_acc(self, preds, targets):
+        return 100 * (preds == targets).float().mean()
