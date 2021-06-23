e656f93a0bb88d656d05632826b8254adbfad39e
wyattlake <wattttte@gmail.com>
2021-06-22 17:41:23 -0700

    Added Neptune Logging and validation

--- model.py
+++ model.py
@@ -4,12 +4,14 @@ import torch
 
 
 class BertSST(pl.LightningModule):
-    def __init__(self, cfg):
+    def __init__(self, cfg, run):
         super().__init__()
         self.model = AutoModelForSequenceClassification.from_pretrained(
             cfg.checkpoint, num_labels=5)
         self.criterion = torch.nn.CrossEntropyLoss()
         self.lr = cfg.learning_rate
+        self.batch_size = cfg.batch_size
+        self.run = run
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
@@ -19,8 +21,32 @@ class BertSST(pl.LightningModule):
         embedding = self.encoder(x)
         return embedding
 
-    def training_step(self, batch):
+    def training_step(self, batch, _batch_idx):
         x, y = batch
         logits = self.model(x).logits
         loss = self.criterion(logits, y.long())
+
+        # Accuracy calculations
+        pred_labels = torch.argmax(logits, axis=1)
+        batch_acc = (pred_labels == y).sum().item()
+
+        # Neptune logging
+        self.run['train/loss'].log(loss.item() / self.batch_size)
+        self.run['train/acc'].log(batch_acc / self.batch_size)
+
+        return loss
+
+    def validation_step(self, batch, _batch_idx):
+        x, y = batch
+        logits = self.model(x).logits
+        loss = self.criterion(logits, y.long())
+
+        # Accuracy calculations
+        pred_labels = torch.argmax(logits, axis=1)
+        batch_acc = (pred_labels == y).sum().item()
+
+        # Neptune logging
+        self.run['eval/loss'].log(loss.item() / self.batch_size)
+        self.run['eval/acc'].log(batch_acc / self.batch_size)
+
         return loss
--- run.py
+++ run.py
@@ -1,30 +1,48 @@
+from pytorch_lightning import Trainer, seed_everything
+from torch.utils.data import DataLoader
 from data import SSTDataset
 from datasets import load_dataset
-from torch.utils.data import DataLoader
-import pytorch_lightning as pl
 from model import BertSST
 import torch
 import hydra
+from dotenv import load_dotenv
+import neptune.new as neptune
+import os
 
 
 @hydra.main(config_path="config", config_name="config")
 def main(cfg):
+    load_dotenv()
+
     # Cuda setup
     device = torch.device(
         "cuda") if torch.cuda.is_available() else torch.device("cpu")
 
+    # Random seeding
+    seed_everything(42, workers=True)
+
     # Dataset loading
     full_dataset = load_dataset("sst", "default")
     train_dataset = SSTDataset(cfg, full_dataset, device, split="train")
-    eval_dataset = SSTDataset(full_dataset, device, split="validation")
+    eval_dataset = SSTDataset(cfg, full_dataset, device, split="validation")
 
-    bert_sst5 = BertSST(cfg)
+    NEPTUNE_TOKEN = os.environ.get("NEPTUNE_TOKEN")
+    run = neptune.init(
+        project='wyattlake/bert-sst5-lightning',
+        api_token=NEPTUNE_TOKEN,
+    )
+
+    bert_sst5 = BertSST(cfg, run)
 
     # Data loaders
-    train_loader = DataLoader(train_dataset)
-    eval_loader = DataLoader(eval_dataset)
+    train_loader = DataLoader(
+        train_dataset, batch_size=cfg.batch_size, shuffle=True)
+    eval_loader = DataLoader(
+        eval_dataset, batch_size=cfg.batch_size, shuffle=False)
 
-    trainer = pl.Trainer()
+    trainer = Trainer(deterministic=True,
+                      accumulate_grad_batches=cfg.accumulation_steps, max_epochs=cfg.epochs, gpus=cfg.gpus)
+
     trainer.fit(bert_sst5, train_loader, eval_loader)
 
 
